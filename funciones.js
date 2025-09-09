455ac57a3250f15aa2074844bd9fdca2dbd57bb4
beretamalr <be.retamalr@duocuc.cl>
2025-09-09 14:58:27 -0300

    Corrección Inicio de Sesión

--- funciones.js
+++ funciones.js
@@ -15,21 +15,26 @@ function iniciarSesion() {
         // Validar usuario y contraseña
         if (usuario === "admin" && contrasena === "1234") {
             alert("Inicio de sesión exitoso");
-            break;
+            window.location.href = "index.html";
+            return true;
         } else if (usuario !== "admin") {
             document.getElementById("errorUsuario").textContent = "Usuario incorrecto";
+            return false;
         } else if (contrasena !== "1234") {
             document.getElementById("errorContrasena").textContent = "Contraseña incorrecta";
+            return false;
         } else if (usuario !== "admin" && contrasena !== "1234") {
             document.getElementById("errorGeneral").textContent = "El usuario o la contraseña son incorrectos";
+            return false;        
         } else if (usuario === "" && contrasena === "") {
             document.getElementById("errorGeneral").textContent = "Por favor, ingrese el usuario y la contraseña";
+            return false;        
         } else if (usuario === "") {
             document.getElementById("errorUsuario").textContent = "Por favor, ingrese el usuario";
+            return false;        
         } else if (contrasena === "") {
             document.getElementById("errorContrasena").textContent = "Por favor, ingrese la contraseña";
+            return false;
         }
     }
-
-    window.location.href = "index.html";
 }
